f32175634d7a8ad4f4649dbdadccdb70d3d6690e
Daniel Collins <mrdanieljcollins@gmail.com>
2022-01-08 19:11:40 -0700

    WIP

--- src/app/page/mission/mission.component.ts
+++ src/app/page/mission/mission.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ExpandBoxComponent, ExpandBoxData } from 'src/app/common/expand-box/expand-box.component';
 
 @Component({
   selector: 'cs-app-mission',
@@ -8,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class MissionComponent implements OnInit {
 
   title: string = 'Mission';
+  missionStatements: BehaviorSubject<ExpandBoxData[]> = new BehaviorSubject<ExpandBoxData[]>([]);
 
   constructor() { }
 
   ngOnInit(): void {
+    const missionStatement: ExpandBoxData = {
+      title: 'Statement of Faith',
+      information: ''
+    };
+
+    this.missionStatements.next([missionStatement])
   }
 
 }
